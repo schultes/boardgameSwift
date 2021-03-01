8aba0d3bbba94242701bebbc0f801c9a42a8c83f
Dominik Schultes <dominik.schultes@iem.thm.de>
2021-03-01 17:33:06 +0100

    Swift-specific implementation of asyncMap added.

--- BoardGame/Model/support/kotlinSupportInSwift.swift
+++ BoardGame/Model/support/kotlinSupportInSwift.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 THM. All rights reserved.
 //
 
+import Foundation
+
 public extension Array {
     func copy() -> Array {
         return self
@@ -30,4 +32,24 @@ func *(lhs: Int, rhs: Double) -> Double {
 
 func -(lhs: Double, rhs: Int) -> Double {
     return lhs - Double(rhs)
+}
+
+// Concurrency
+extension Array {
+    func asyncMap<R>(transform: @escaping (Element) -> R, processResult: @escaping ([R]) -> ()) {
+        let queue = DispatchQueue(label: "SequalsKconcurrency", attributes: .concurrent)
+        let group = DispatchGroup()
+        var result = [R?](repeating: nil, count: self.count)
+        
+        for i in 0..<self.count {
+            let index = i // it is crucial to make this copy of the index
+            queue.async(group: group) {
+                result[index] = transform(self[index])
+            }
+        }
+        
+        group.notify(queue: .main) {
+            processResult(result.map {$0!})
+        }
+    }
 }
